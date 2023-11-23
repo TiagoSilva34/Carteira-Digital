436c5695d3a1f8b1998612c7d07c7273aca7be12
Tiago Silva <tiagoborges899@gmail.com>
2023-11-23 05:16:19 -0300

    feat: Foamtação dos valores do gráfico de linha

--- src/components/HistoryBox/index.tsx
+++ src/components/HistoryBox/index.tsx
@@ -5,6 +5,9 @@ import {
   LegendContainer,
   Legend 
 } from './styles';
+
+import { formatCurrency } from '../../utils/formatCurrency';
+
 import {
   ResponsiveContainer,
   LineChart,
@@ -56,7 +59,7 @@ export const HistoryBox: React.FC<IHistoryBoxProps> = ({
         >
           <CartesianGrid strokeDasharray='3 3' stroke='#cecece' />
           <XAxis dataKey='month' stroke='#cecece' />
-          <Tooltip />
+          <Tooltip formatter={(value) => formatCurrency(Number(value))}/>
           <Line
             type='monotone'
             dataKey='amountEntry'
